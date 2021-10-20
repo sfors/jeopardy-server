36457049a902c248bd70952d02e4eb7f9811fc1b
Simon Risberg <sirsimon40@gmail.com>
2021-10-20 10:21:44 +0200

    Saved this for later

--- src/jepordy/server/main.clj
+++ src/jepordy/server/main.clj
@@ -2,4 +2,18 @@
 
 (range 10)
 
-(print "Ekholm")
+(->> (range 10)
+     (filter odd?))
+
+(def m {"a", "1", "b", 2})
+
+(assoc m "c" 42)
+
+(type "a")
+
+(.getClass "a")
+
+(def a (atom {"a" 1}))
+
+(def time1 (deref a))
+
