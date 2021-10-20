6fe4ddec3f947d3b729f5bfb8f3e20845f3519d1
Simon Risberg <sirsimon40@gmail.com>
2021-10-20 10:58:55 +0200

    Server created

--- src/jepordy/server/main.clj
+++ src/jepordy/server/main.clj
@@ -1,19 +1,37 @@
-(ns jepordy.server.main)
+(ns jepordy.server.main
+  (:require [org.httpkit.server :as http-server]
+            [clojure.pprint :refer [pprint]]))
 
-(range 10)
+(defonce server-atom (atom nil))
 
-(->> (range 10)
-     (filter odd?))
+(defn handler! [request]
+  (pprint request)
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "<h1>Innovation day</h1>"})
 
-(def m {"a", "1", "b", 2})
+(defn server-started?
+  []
+  (deref server-atom))
 
-(assoc m "c" 42)
+(defn start-server!
+  []
+  ((when-not server-started?))
+  (reset! server-atom
+          (http-server/run-server (fn [request]
+                                    (handler! request))
+                                  {:port 7000})))
 
-(type "a")
 
-(.getClass "a")
+(defn stop-server!
+  []
+  (when (server-started?))
+  (let [stop-server-fn (deref server-atom)]
+    (stop-server-fn :timeout 100)
+    reset! server-atom nil))
 
-(def a (atom {"a" 1}))
-
-(def time1 (deref a))
-
+(comment
+  (start-server!)
+  (stop-server!)
+  (server-started?)
+  )
