24000ee84c93a00d945901e5536abbe1b9f08653
Tomas Ekholm <tomas.ekholm@tietoevry.com>
2021-10-20 16:10:02 +0200

    Added websockets

--- src/jeopardy/server/endpoints.clj
+++ src/jeopardy/server/endpoints.clj
@@ -1,6 +1,59 @@
 (ns jeopardy.server.endpoints
   (:require [clojure.pprint :refer [pprint]]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [org.httpkit.server :as http-server]))
+
+(defonce user->channels-atom (atom {}))
+
+(defonce state-atom (atom {:users {}}))
+
+(comment
+  (deref state-atom)
+  (deref user->channels-atom)
+  )
+
+(defn new-connection
+  [channel name]
+  (println "New connection:" name)
+  (swap! user->channels-atom (fn [user->channel]
+                               (assoc user->channel name channel)))
+  (swap! state-atom assoc-in [:users name] {})
+  (let [user->channels (deref user->channels-atom)]
+    (doseq [channel (vals user->channels)]
+      (http-server/send! channel
+                         (str {:data {:event :new-connection
+                                      :names (keys user->channels)}})))))
+
+
+(defn receive-new-message
+  [name text]
+  (doseq [channel (:channels (deref channels-atom))]
+    (http-server/send! channel (str "Some message got to the server"))))
+
+
+(defn receive-data-from-channel
+  [channel data]
+  (println "Received data from client")
+  (let [{action :action name :name text :text} (read-string data)]
+    (println "Action" action "Name" name "Text" text)
+    (condp = action
+      :connect (new-connection channel name)
+      :new-message (receive-new-message name text))))
+
+(defn websocket-handler!
+  [request]
+  (println "Websocket request received!")
+  (http-server/with-channel
+    request channel
+    (http-server/on-close channel
+                          (fn [status]
+                            (println "channel closed: " status)))
+    (http-server/on-receive channel
+                            (fn [data]
+                              (receive-data-from-channel channel data)))))
+
+(defn http-handler! [request]
+  (println "HTTP request received!")
 
 (defn get-headers
   []
@@ -16,8 +69,6 @@
         body (when (:body request)
                (slurp (:body request)))]
 
-    ;(println (json/read-str body :key-fn keyword))
-
     (cond (= uri "/to-upper-case")
           {:status  200
            :headers (get-headers)
@@ -28,3 +79,10 @@
            :headers (get-headers)
            :body    "{\"a\": 42}"}))
   )
+
+(defn handler!
+  [request]
+  (pprint request)
+  (if (:websocket? request)
+    (websocket-handler! request)
+    (http-handler! request)))
